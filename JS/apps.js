c5fac4514207c24e9cfbcf09e076812e936c8e9a
James Kim <hontailhd@gmail.com>
2021-04-04 19:10:10 -0700

    added template for JS codeing

---
+++ JS/apps.js
@@ -0,0 +1,55 @@
+
+
+
+
+/* function for theme changes */
+    /*Adds a dark button for your app */
+        
+        /* Changes the mode of the app to dark/light */
+        
+        /* button will change to notify user the dark/light theme is reversable */
+
+        
+/* function to add a new note */
+    /* create button */
+        /* Creates a note-taking area*/
+            /* Can be a textarea or div */
+
+        /* Creates a save button*/
+            /* saves note in note taking area when clicked*/
+
+            /* Removes note takeing area */
+
+            /* Removes save button*/
+
+            /* Removes cancel button */
+
+            /* Asks user for note-title */
+
+        /* Creates a cancel button*/
+
+            /* Removes text not taking area*/
+
+            /* Removes note takeing area */
+
+            /* Removes save button*/
+
+            /* Removes cancel button */
+
+
+
+
+
+/* function to add not into array */
+    /* ask for note-title */
+
+    /* note-title is added to noteArray */
+
+    /* the note-title turns into a button */
+        /* button displays note with the note-title */
+        
+        /* Button shows close-note-button */
+            /* close-note-button hides the displayed-note */
+             
+            /* close-not-button hides close-note-button */
+
